b47c404ec64448c4a3fded3d815b78ad684c9d6d
Lssikkes <seniltai@gmail.com>
2020-05-24 13:15:56 +0200

    Update turboredis.lua

--- turboredis.lua
+++ turboredis.lua
@@ -1,3 +1,19 @@
+-- make sure string.split is defined, turbo-redis requires it to be there.
+if string.split == nil then
+  function string:split(delimiter)
+    local result = { }
+    local from  = 1
+    local delim_from, delim_to = string.find( self, delimiter, from  )
+    while delim_from do
+      table.insert( result, string.sub( self, from , delim_from-1 ) )
+      from  = delim_to + 1
+      delim_from, delim_to = string.find( self, delimiter, from  )
+    end
+    table.insert( result, string.sub( self, from  ) )
+    return result
+  end
+end
+
 local turboredis = {
     SORT_DESC = "desc",
     SORT_ASC = "asc",
